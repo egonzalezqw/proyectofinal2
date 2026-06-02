6ab06e756b7075b5b44d770274154cc8a423e508
egonzalezqw <egofac@gmail.com>
2026-06-01 18:54:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,72 +1,27 @@
-import streamlit as st
+st.header("🏝️ Misión 1")
 
-# Configuración de la página
-st.set_page_config(
-    page_title="Linux Challenge",
-    page_icon="🐧"
+st.code("""
+#!/bin/bash
+
+_______="Linux"
+
+echo $curso
+""", language="bash")
+
+respuesta = st.text_input(
+    "¿Qué palabra falta?"
 )
 
-# Variables de sesión
-if "nivel" not in st.session_state:
-    st.session_state.nivel = 1
+if st.button("Validar"):
 
-# Título
-st.title("🐧 Linux Survival Challenge")
+    if respuesta.strip().lower() == "curso":
 
-# Mostrar nivel actual
-st.write(f"Nivel actual: {st.session_state.nivel}")
+        st.success("Correcto")
 
-# Nivel 1
-if st.session_state.nivel == 1:
+        st.session_state.nivel += 1
 
-    st.header("Misión 1")
+        st.rerun()
 
-    st.write("""
-    Cree un script llamado bienvenida.sh que muestre:
+    else:
 
-    Bienvenido al Linux Challenge
-    """)
-
-    respuesta = st.text_input(
-        "¿Qué mostró el script?"
-    )
-
-    if st.button("Validar"):
-
-        if respuesta == "Bienvenido al Linux Challenge":
-
-            st.success("Correcto")
-
-            st.session_state.nivel = 2
-
-            st.rerun()
-
-        else:
-
-            st.error("Respuesta incorrecta")
-
-# Nivel 2
-elif st.session_state.nivel == 2:
-
-    st.header("Misión 2")
-
-    st.write("""
-    Cree un script que utilice una variable llamada nombre
-    y la muestre utilizando echo.
-    """)
-
-    respuesta = st.text_input(
-        "¿Qué valor mostró?"
-    )
-
-    if st.button("Validar Nivel 2"):
-
-        if respuesta.strip() != "":
-
-            st.success("¡Proyecto completado!")
-
-            st.balloons()
-
-        else:
-
-            st.error("Ingrese una respuesta")
+        st.error("Incorrecto")
