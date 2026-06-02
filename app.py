12b168761a09308e53382796733d7e5efd6407e6
egonzalezqw <egofac@gmail.com>
2026-06-01 18:58:28 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Linux Survival Challenge",
-    page_icon="🐧"
-)
+st.title("🐧 Linux Bash Challenge")
 
-st.title("🐧 Linux Survival Challenge")
+if "nivel" not in st.session_state:
+    st.session_state.nivel = 1
 
-st.header("🏝️ Misión 1")
+st.write(f"Nivel actual: {st.session_state.nivel}")
 
-st.code("""
+# MISIÓN 1
+if st.session_state.nivel == 1:
+
+    st.header("🏝️ Misión 1")
+
+    st.code("""
 #!/bin/bash
 
 _______="Linux"
@@ -17,13 +20,45 @@ _______="Linux"
 echo $curso
 """, language="bash")
 
-respuesta = st.text_input(
-    "¿Qué palabra falta?"
-)
+    respuesta = st.text_input(
+        "¿Qué palabra falta?"
+    )
 
-if st.button("Validar"):
+    if st.button("Validar Misión 1"):
 
-    if respuesta.lower().strip() == "curso":
-        st.success("✅ Correcto")
-    else:
-        st.error("❌ Incorrecto")
+        if respuesta.lower().strip() == "curso":
+
+            st.success("✅ Correcto")
+
+            st.session_state.nivel = 2
+
+            st.rerun()
+
+        else:
+
+            st.error("❌ Incorrecto")
+
+# MISIÓN 2
+elif st.session_state.nivel == 2:
+
+    st.header("🏕️ Misión 2")
+
+    st.code("""
+#!/bin/bash
+
+echo "___________"
+""", language="bash")
+
+    respuesta = st.text_input(
+        "Complete el mensaje"
+    )
+
+    if st.button("Validar Misión 2"):
+
+        if respuesta.strip() == "Bienvenido a Linux":
+
+            st.success("🎉 Pasaste la misión 2")
+
+        else:
+
+            st.error("❌ Incorrecto")
